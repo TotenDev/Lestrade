ca62e724df12e5a9e308b16b85a15e9ba676beb9
Marcos Trovilho <marcos@trovilho.com>
2012-10-23 19:27:26 -0200

    ssl and auth on production only
    now ssl redirect and basic authentication only happen in production.
    add a method to get libLestrade version from request headers

--- lestrade.rb
+++ lestrade.rb
@@ -2,10 +2,22 @@ require 'sinatra'
 require 'rack-ssl-enforcer'
 require_relative 'lib/iap_validator'
 
-use Rack::SslEnforcer
+configure :production do
+  use Rack::SslEnforcer
 
-use Rack::Auth::Basic do |username, password|
-  [username, password] == [ENV['LESTRADE_USERNAME'], ENV['LESTRADE_PASSWORD']]
+  use Rack::Auth::Basic do |username, password|
+    [username, password] == [ENV['LESTRADE_USERNAME'], ENV['LESTRADE_PASSWORD']]
+  end
+end
+
+helpers do
+  def validate( receipt, sandbox=false )
+    { status: Lestrade::IAPValidator.valid?( receipt, sandbox ) }
+  end
+
+  def lib_version
+    request.env['HTTP_X_LIBLESTRADE_VERSION']
+  end
 end
 
 post '/validate' do
@@ -18,8 +30,4 @@ post '/sandbox/validate' do
   content_type :json
   data = JSON.parse( request.body.read )
   validate( data['receipt-data'], true ).to_json
-end
-
-def validate( receipt, sandbox=false )
-  { status: Lestrade::IAPValidator.valid?( receipt, sandbox ) }
 end
